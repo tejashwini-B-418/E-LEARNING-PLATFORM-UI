912fc9d6380cfb85c89e033c949da844173033d0
Tejashwini B <s85697783@gmail.com>
2025-11-25 00:17:00 +0530

    Add files via upload

---
+++ SkillBoost Academy/assests/js/script.js
@@ -0,0 +1,9 @@
+console.log("SkillBoost Academy Loaded");
+
+function showWelcomeMessage() {
+  alert("Welcome back to SkillBoost Academy!");
+}
+
+function showCertificateAlert() {
+  alert("Complete 100% progress to unlock your certificate!");
+}
